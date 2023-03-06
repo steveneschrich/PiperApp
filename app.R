83291a1a8d419d837ec9d686972195f2494ef498
Steven Eschrich <35569991+steveneschrich@users.noreply.github.com>
2023-03-05 19:26:36 -0500

    Updated app reflecting changes to underlying piper library.

--- app.R
+++ app.R
@@ -9,6 +9,7 @@
 
 # Load the data we will be working from in the application.
 lscc <- readRDS(here::here("data/lscc.rds"))
+panels <- readRDS(here::here("data/panels.rds"))
 
 clinical_panel <- function(file = "lscc.rds") {
   lscc
@@ -22,43 +23,35 @@ library(magrittr)
 ui <- fluidPage(
   #theme = bslib::bs_theme(version = 5, bootswatch = "minty"),
   # App title ----
-  titlePanel("PIPER" ),
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
+  fluidRow(
 
     # Sidebar panel for inputs ----
-    sidebarPanel(
-      shiny::imageOutput("hdr_image", width="1%", height="1px", inline=TRUE),
-      shiny::selectInput("patient", "Patient:",
-                         Biobase::sampleNames(clinical_panel())
-      ),
+    column(width=1,
+           shiny::imageOutput("hdr_image", width="1%", height="1px", inline=TRUE),
 
+    ),
+    column(width=2,titlePanel("PIPER" ))
+  ),
+
+  # Sidebar layout with input and output definitions ----
+  fluidRow(
+
+    column(width=6,
+           shiny::selectInput("patient", "Patient:",
+                              colnames(clinical_panel())
+           )
+    ),
+    column(width=6,
       shiny::selectInput(
         "panel", "Clinical Panel:",
-        unique(Biobase::fData(clinical_panel())$Subcategory)
+        names(panels)
       )
-    ),
-    # Input: Select the random distribution type ----
-    #   radioButtons("dist", "Distribution type:",
-    #                c("Normal" = "norm",
-    #                  "Uniform" = "unif",
-    #                  "Log-normal" = "lnorm",
-    #                  "Exponential" = "exp")),
-    #
-    #   # br() element to introduce extra vertical spacing ----
-    #   br(),
-    #
-    #   # Input: Slider for the number of observations to generate ----
-    #   sliderInput("n",
-    #               "Number of observations:",
-    #               value = 500,
-    #               min = 1,
-    #               max = 1000)
-    #
-    # ),
+    )
+  ),
+  fluidRow(
+    column(width=12,
+      # Main panel for displaying outputs ----
 
-    # Main panel for displaying outputs ----
-    mainPanel(
       #theme = bslib::bs_theme(version = 5, bootswatch = "minty"),
       # Output: Tabset w/ plot, summary, and table ----
       tabsetPanel(type = "tabs",
@@ -67,26 +60,37 @@ ui <- fluidPage(
                                     shiny::tableOutput("overviewTable"), shiny::plotOutput("subwayMap"))
                   ),
                   shiny::tabPanel("Panel Measurements",reactable::reactableOutput("summaryTable")),
-                  shiny::tabPanel("Spider", shiny::plotOutput("spiderPlot")),
+                  shiny::tabPanel("Radar", plotly::plotlyOutput("radarPlot")),
                   shiny::tabPanel("Donut", shiny::plotOutput("donutPlot")),
                   shiny::tabPanel("Lollipop", shiny::plotOutput("lollipop")),
                   shiny::tabPanel("Rug Plot", shiny::plotOutput("rugplot", inline=FALSE)),
-                  shiny::tabPanel("Heatmap", shiny::plotOutput("heatmap"))
+                  shiny::tabPanel(
+                    "Heatmap",
+                    shiny::plotOutput("heatmap")
+                  )
       )
 
     )
   )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-  output$spiderPlot <- shiny::renderPlot({
-    piper::plot_spider(clinical_panel(), pnum=input$patient, panel=input$panel)
+# Define server logic required to draw a histogram
+server <- function(input, output,session) {
+
+  output$radarPlot <- plotly::renderPlotly({
+    piper::plot_radar(
+      clinical_panel()[,input$patient],
+      clinical_panel(),
+      panel=panels[[input$panel]]
+    )
   })
   output$donutPlot <- shiny::renderPlot({
-    piper::plot_donut(clinical_panel(), pnum=input$patient, panel=input$panel)
-
+    piper::plot_donut(
+      clinical_panel()[,input$patient],
+      clinical_panel(),
+      panel=panels[[input$panel]]
+    )
   })
   output$hdr_image <- shiny::renderImage({
     list(src=here::here("img/PIPER_IMG.png"),width="50px",height="50px",alt="PIPER logo")
@@ -95,23 +99,39 @@ server <- function(input, output) {
   )
 
   output$summaryTable <- reactable::renderReactable({
-    reactable::reactable(table_panel_statistics(clinical_panel(), pnum=input$patient, panel=input$panel))
+    reactable::reactable(table_panel_statistics(clinical_panel(), pnum=input$patient))
   })
   output$summary<-shiny::renderText("Summary here")
   output$overviewTable <- shiny::renderTable({
     overview_stats(clinical_panel())
   })
   output$heatmap <- shiny::renderPlot({
-    piper::plot_heatmap(clinical_panel(), pnum=input$patient, panel=input$panel)
+
+    ht1 <- piper::plot_heatmap(
+      sample = clinical_panel()[,input$patient],
+      reference = clinical_panel(),
+      panel = panels[[input$panel]]
+    )
+   ht1
+
   })
   output$lollipop <- shiny::renderPlot({
-    piper::plot_lollipop(clinical_panel(), pnum=input$patient, panel=input$panel)
+    piper::plot_lollipop(
+      sample = clinical_panel()[,input$patient],
+      reference=clinical_panel(),
+      panel=panels[[input$panel]])
   })
   output$rugplot <- shiny::renderPlot({
-    piper::plot_rug(clinical_panel(), pnum=input$patient, panel=input$panel)
+    piper::plot_rug(
+      sample = clinical_panel()[,input$patient],
+      reference = clinical_panel(),
+      panel=panels[[input$panel]]
+    )
   })
   output$subwayMap <- shiny::renderPlot({
-    piper::plot_subway(clinical_panel(), pnum=input$patient)
+    piper::plot_subway(clinical_panel()[,input$patient],
+                       clinical_panel(),
+                       panels)
   })
 }
 
@@ -119,27 +139,18 @@ server <- function(input, output) {
 #'
 #' @param .x
 #' @param pnum
-#' @param panel
 #'
 #' @return
 #' @export
 #'
 #' @examples
-table_panel_statistics <- function(.x, pnum=1, panel="Tissue QC") {
+table_panel_statistics <- function(.x, pnum=1) {
 
 
   if ( is.character(pnum))
-    pnum <- which(Biobase::sampleNames(.x) %in% pnum)
+    pnum <- which(colnames(.x) %in% pnum)
 
-  piper::get_sample(.x, pnum) %>%
-    dplyr::left_join(piper::calculate_panel_statistics(.x), by="Protein_Peptide") %>%
-    dplyr::mutate(Quantile = purrr::map2_dbl(.data$ECDF, .data$Patient, ~100 * .x(.y))) %>%
-    dplyr::arrange(.data$Subcategory, .data$Protein_Peptide) %>%
-    dplyr::select(.data$Protein_Peptide, .data$Patient,
-                  .data$Subcategory, .data$Quantile,
-                  .data$MinExpression, .data$MeanExpression,
-                  .data$MaxExpression)
-
+  piper::summary_statistics_table(.x[,pnum], .x, panels)
 }
 
 
@@ -154,9 +165,9 @@ table_panel_statistics <- function(.x, pnum=1, panel="Tissue QC") {
 overview_stats <- function(.x) {
   tibble::tribble(
     ~Description, ~Value,
-    "Samples", unname(ncol(.x)),
-    "Assays", unname(nrow(.x)),
-    "Assay Categories", length(unique(Biobase::fData(.x)$Category))
+    "Samples", ncol(.x),
+    "Assays", nrow(.x),
+    "Panels", length(panels)
   )
 }
 
